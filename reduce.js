688c9adb67df939513efdf19b2cd1388f30a35eb
Gustavo Saboya <saboya.gustavo@gmail.com>
2020-11-21 16:31:37 -0300

    using array.prototype.reduce() to return an object

--- reduce.js
+++ reduce.js
@@ -4,37 +4,48 @@ const pets = [
         name: 'bud',
         breed: 'labrador',
         age: 7,
+        weight: 31,
     },
     {
         kind: 'hamster',
         name: 'fluffy',
         breed: 'dwarf',
         age: 2,
+        weight: 0.04,
     },
     {
         kind: 'fish',
         name: 'goldie',
         breed: 'goldfish',
         age: 11,
+        weight: 0.8,
     },
     {
         kind: 'cat',
         name: 'moustache',
         breed: 'siamese',
         age: 8,
+        weight: 3.2,
     },
     {
         kind: 'dog',
         name: 'k9',
         breed: 'german shepherd',
         age: 6,
+        weight: 35,
     },
 ];
 
-const totalPetAges = pets.reduce((acc, cur) => {
-    // console.log(`Total: ${acc}`);
-    // console.log(`Pet: ${cur.name}`);
-    return acc + cur.age;
-}, 0);
+const petAgesAndWeight = pets.reduce(
+    (acc, cur) => {
+        // console.log(acc); -> debug
+        return {
+            totalAges: acc.totalAges + cur.age,
+            totalWeight: acc.totalWeight + cur.weight,
+        };
+    },
+    { totalAges: 0, totalWeight: 0 }
+);
 
-console.log(totalPetAges);
+console.log(`somatória de idade dos pets é: ${petAgesAndWeight.totalAges} anos.`);
+console.log(`somatória de peso dos pets é: ${petAgesAndWeight.totalWeight} quilos.`);
